3705a62f216e9a3897d91b18928fb6dcbef8f54a
Ankit Singh <155769995+Ankit-singhtest@users.noreply.github.com>
2024-03-23 19:49:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/* create a business name by combining list of adjectives and shop name and other word 
+
+  Adjectives:
+  crazy
+  amazing 
+  fire
+
+  Shop name:
+  engine 
+  foods 
+  garments
+
+  Another word
+  Bros 
+  limited 
+  hub
+  */
